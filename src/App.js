1528aaf5ed5a814cca9353b0c202f890be722556
Shriyanksoni2 <rs1509747@gmail.com>
2025-08-27 20:20:05 +0530

    portfolio

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css'
+import logo from './assets/img/logo.svg'
+import {NavBar} from './components/NavBar'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <NavBar/>
+       </div>
+    </>
+  )
+}
+
+export default App
